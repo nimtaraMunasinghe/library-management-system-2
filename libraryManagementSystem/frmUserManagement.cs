79e6d3b733ae5df46774d61ed26663e8c16b921b
nimtara <nimtaramunasinghe@gmail.com>
2021-06-25 14:47:23 +0530

    Add button functions

--- libraryManagementSystem/frmUserManagement.cs
+++ libraryManagementSystem/frmUserManagement.cs
@@ -51,5 +51,90 @@ namespace libraryManagementSystem
 
             clear();
         }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query_updte = "update tblUser set user_ID = '" + txtUserID.Text + "', user_name = '" + txtName.Text + "', user_password = '" + txtPassword.Text + "', user_type = '" + txtUserType.Text + "', user_address = '" + txtAddress.Text + "', user_telephone = '" + txtContactNumber.Text + "' where user_ID = '"+txtUserID.Text+"'";
+                SqlCommand cmd = new SqlCommand(query_updte, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("User updated successfully!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error while updating user" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query_delete = "delete from tblUser where user_ID = '" + txtUserID.Text + "'";
+                SqlCommand cmd = new SqlCommand(query_delete, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("User deleted successfully!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error while deleting user" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query_search = "select * from tblUser where user_ID = '" + txtUserID.Text + "'";
+                SqlCommand cmd = new SqlCommand(query_search, con);
+                con.Open();
+                SqlDataReader r = cmd.ExecuteReader();
+
+                while (r.Read())
+                {
+                    txtUserID.Text = r[0].ToString();
+                    txtName.Text = r[1].ToString();
+                    txtPassword.Text = r[2].ToString();
+                    txtUserType.Text = r[3].ToString();
+                    txtAddress.Text = r[4].ToString();
+                    txtContactNumber.Text = r[5].ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error while searching user " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            frmHome home = new frmHome();
+            home.Visible = true;
+            this.Visible = false;
+        }
     }
 }

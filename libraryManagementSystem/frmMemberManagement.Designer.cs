721cf1ad19053e7a7457cd21387771ed8d99ddd8
nimtara <nimtaramunasinghe@gmail.com>
2021-06-27 15:12:36 +0530

    complete basic functions

--- libraryManagementSystem/frmMemberManagement.Designer.cs
+++ libraryManagementSystem/frmMemberManagement.Designer.cs
@@ -29,19 +29,18 @@ namespace libraryManagementSystem
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMemberManagement));
             this.btnHome = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
-            this.btnSearch = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.txtMemberID = new System.Windows.Forms.TextBox();
+            this.txtGuardianName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtContactNumber = new System.Windows.Forms.TextBox();
+            this.txtMemberName = new System.Windows.Forms.TextBox();
+            this.lblGurdian = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -51,117 +50,115 @@ namespace libraryManagementSystem
             this.lblUserMM = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lblUserTypeMM = new System.Windows.Forms.Label();
+            this.pnlMemberType = new System.Windows.Forms.Panel();
+            this.rbtnStudent = new System.Windows.Forms.RadioButton();
+            this.rbtnStaff = new System.Windows.Forms.RadioButton();
+            this.btnSearchMemberID = new System.Windows.Forms.Button();
+            this.btnSearchMemberName = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlMemberType.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(498, 618);
+            this.btnHome.Location = new System.Drawing.Point(330, 566);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(94, 29);
             this.btnHome.TabIndex = 41;
             this.btnHome.Text = "Home";
             this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(330, 618);
+            this.btnDelete.Location = new System.Drawing.Point(498, 510);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(94, 29);
             this.btnDelete.TabIndex = 40;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(172, 618);
+            this.btnUpdate.Location = new System.Drawing.Point(330, 510);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(94, 29);
             this.btnUpdate.TabIndex = 39;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Location = new System.Drawing.Point(498, 553);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(94, 29);
-            this.btnSearch.TabIndex = 38;
-            this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(330, 553);
+            this.btnClear.Location = new System.Drawing.Point(172, 566);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(94, 29);
             this.btnClear.TabIndex = 37;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(172, 553);
+            this.btnAdd.Location = new System.Drawing.Point(172, 510);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(94, 29);
             this.btnAdd.TabIndex = 36;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // textBox6
+            // txtMemberID
             // 
-            this.textBox6.Location = new System.Drawing.Point(310, 205);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(271, 22);
-            this.textBox6.TabIndex = 35;
+            this.txtMemberID.Location = new System.Drawing.Point(310, 205);
+            this.txtMemberID.Name = "txtMemberID";
+            this.txtMemberID.Size = new System.Drawing.Size(271, 22);
+            this.txtMemberID.TabIndex = 35;
             // 
-            // textBox5
+            // txtGuardianName
             // 
-            this.textBox5.Location = new System.Drawing.Point(310, 456);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(271, 22);
-            this.textBox5.TabIndex = 34;
+            this.txtGuardianName.Location = new System.Drawing.Point(310, 432);
+            this.txtGuardianName.Name = "txtGuardianName";
+            this.txtGuardianName.Size = new System.Drawing.Size(271, 22);
+            this.txtGuardianName.TabIndex = 34;
+            this.txtGuardianName.Visible = false;
             // 
-            // textBox4
+            // txtAddress
             // 
-            this.textBox4.Location = new System.Drawing.Point(310, 403);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(271, 22);
-            this.textBox4.TabIndex = 33;
+            this.txtAddress.Location = new System.Drawing.Point(310, 334);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(271, 22);
+            this.txtAddress.TabIndex = 32;
             // 
-            // textBox3
+            // txtContactNumber
             // 
-            this.textBox3.Location = new System.Drawing.Point(310, 352);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(271, 22);
-            this.textBox3.TabIndex = 32;
+            this.txtContactNumber.Location = new System.Drawing.Point(310, 290);
+            this.txtContactNumber.Name = "txtContactNumber";
+            this.txtContactNumber.Size = new System.Drawing.Size(271, 22);
+            this.txtContactNumber.TabIndex = 31;
             // 
-            // textBox2
+            // txtMemberName
             // 
-            this.textBox2.Location = new System.Drawing.Point(310, 304);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(271, 22);
-            this.textBox2.TabIndex = 31;
+            this.txtMemberName.Location = new System.Drawing.Point(310, 248);
+            this.txtMemberName.Name = "txtMemberName";
+            this.txtMemberName.Size = new System.Drawing.Size(271, 22);
+            this.txtMemberName.TabIndex = 30;
             // 
-            // textBox1
+            // lblGurdian
             // 
-            this.textBox1.Location = new System.Drawing.Point(310, 259);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(271, 22);
-            this.textBox1.TabIndex = 30;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(114, 461);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(108, 17);
-            this.label7.TabIndex = 29;
-            this.label7.Text = "Guardian Name";
+            this.lblGurdian.AutoSize = true;
+            this.lblGurdian.Location = new System.Drawing.Point(114, 437);
+            this.lblGurdian.Name = "lblGurdian";
+            this.lblGurdian.Size = new System.Drawing.Size(108, 17);
+            this.lblGurdian.TabIndex = 29;
+            this.lblGurdian.Text = "Guardian Name";
+            this.lblGurdian.Visible = false;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(114, 408);
+            this.label6.Location = new System.Drawing.Point(114, 381);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(95, 17);
             this.label6.TabIndex = 28;
@@ -170,7 +167,7 @@ namespace libraryManagementSystem
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(114, 357);
+            this.label5.Location = new System.Drawing.Point(114, 339);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(60, 17);
             this.label5.TabIndex = 27;
@@ -179,7 +176,7 @@ namespace libraryManagementSystem
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(114, 309);
+            this.label4.Location = new System.Drawing.Point(114, 295);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(110, 17);
             this.label4.TabIndex = 26;
@@ -188,7 +185,7 @@ namespace libraryManagementSystem
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(114, 264);
+            this.label3.Location = new System.Drawing.Point(114, 253);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 17);
             this.label3.TabIndex = 25;
@@ -239,25 +236,90 @@ namespace libraryManagementSystem
             this.lblUserTypeMM.TabIndex = 42;
             this.lblUserTypeMM.Text = "User Type";
             // 
+            // pnlMemberType
+            // 
+            this.pnlMemberType.Controls.Add(this.rbtnStudent);
+            this.pnlMemberType.Controls.Add(this.rbtnStaff);
+            this.pnlMemberType.Location = new System.Drawing.Point(310, 362);
+            this.pnlMemberType.Name = "pnlMemberType";
+            this.pnlMemberType.Size = new System.Drawing.Size(271, 52);
+            this.pnlMemberType.TabIndex = 45;
+            // 
+            // rbtnStudent
+            // 
+            this.rbtnStudent.AutoSize = true;
+            this.rbtnStudent.Location = new System.Drawing.Point(20, 15);
+            this.rbtnStudent.Name = "rbtnStudent";
+            this.rbtnStudent.Size = new System.Drawing.Size(78, 21);
+            this.rbtnStudent.TabIndex = 46;
+            this.rbtnStudent.TabStop = true;
+            this.rbtnStudent.Text = "Student";
+            this.rbtnStudent.UseVisualStyleBackColor = true;
+            this.rbtnStudent.CheckedChanged += new System.EventHandler(this.rbtnStudent_CheckedChanged);
+            // 
+            // rbtnStaff
+            // 
+            this.rbtnStaff.AutoSize = true;
+            this.rbtnStaff.Location = new System.Drawing.Point(148, 15);
+            this.rbtnStaff.Name = "rbtnStaff";
+            this.rbtnStaff.Size = new System.Drawing.Size(58, 21);
+            this.rbtnStaff.TabIndex = 47;
+            this.rbtnStaff.TabStop = true;
+            this.rbtnStaff.Text = "Staff";
+            this.rbtnStaff.UseVisualStyleBackColor = true;
+            this.rbtnStaff.CheckedChanged += new System.EventHandler(this.rbtnStaff_CheckedChanged);
+            // 
+            // btnSearchMemberID
+            // 
+            this.btnSearchMemberID.Image = ((System.Drawing.Image)(resources.GetObject("btnSearchMemberID.Image")));
+            this.btnSearchMemberID.Location = new System.Drawing.Point(596, 200);
+            this.btnSearchMemberID.Name = "btnSearchMemberID";
+            this.btnSearchMemberID.Size = new System.Drawing.Size(37, 30);
+            this.btnSearchMemberID.TabIndex = 46;
+            this.btnSearchMemberID.UseVisualStyleBackColor = true;
+            this.btnSearchMemberID.Click += new System.EventHandler(this.btnSearchMemberID_Click);
+            // 
+            // btnSearchMemberName
+            // 
+            this.btnSearchMemberName.Image = ((System.Drawing.Image)(resources.GetObject("btnSearchMemberName.Image")));
+            this.btnSearchMemberName.Location = new System.Drawing.Point(596, 248);
+            this.btnSearchMemberName.Name = "btnSearchMemberName";
+            this.btnSearchMemberName.Size = new System.Drawing.Size(37, 30);
+            this.btnSearchMemberName.TabIndex = 48;
+            this.btnSearchMemberName.UseVisualStyleBackColor = true;
+            this.btnSearchMemberName.Click += new System.EventHandler(this.btnSearchMemberName_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(498, 566);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 29);
+            this.btnExit.TabIndex = 49;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
             // frmMemberManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 695);
+            this.ClientSize = new System.Drawing.Size(785, 625);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearchMemberName);
+            this.Controls.Add(this.btnSearchMemberID);
+            this.Controls.Add(this.pnlMemberType);
             this.Controls.Add(this.lblUserTypeMM);
             this.Controls.Add(this.btnHome);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMemberID);
+            this.Controls.Add(this.txtGuardianName);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtContactNumber);
+            this.Controls.Add(this.txtMemberName);
+            this.Controls.Add(this.lblGurdian);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -268,6 +330,9 @@ namespace libraryManagementSystem
             this.Controls.Add(this.label1);
             this.Name = "frmMemberManagement";
             this.Text = "Member Management";
+            this.Load += new System.EventHandler(this.frmMemberManagement_Load);
+            this.pnlMemberType.ResumeLayout(false);
+            this.pnlMemberType.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -278,16 +343,14 @@ namespace libraryManagementSystem
         private System.Windows.Forms.Button btnHome;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnUpdate;
-        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMemberID;
+        private System.Windows.Forms.TextBox txtGuardianName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtContactNumber;
+        private System.Windows.Forms.TextBox txtMemberName;
+        private System.Windows.Forms.Label lblGurdian;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
@@ -297,5 +360,11 @@ namespace libraryManagementSystem
         public System.Windows.Forms.Label lblUserMM;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.Label lblUserTypeMM;
+        private System.Windows.Forms.Panel pnlMemberType;
+        private System.Windows.Forms.RadioButton rbtnStudent;
+        private System.Windows.Forms.RadioButton rbtnStaff;
+        private System.Windows.Forms.Button btnSearchMemberID;
+        private System.Windows.Forms.Button btnSearchMemberName;
+        private System.Windows.Forms.Button btnExit;
     }
 }
--- libraryManagementSystem/frmMemberManagement.cs
+++ libraryManagementSystem/frmMemberManagement.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace libraryManagementSystem
 {
@@ -15,6 +16,245 @@ namespace libraryManagementSystem
         public frmMemberManagement()
         {
             InitializeComponent();
+        }
+
+        public void clear()
+        {
+            txtMemberID.Clear();
+            txtMemberName.Clear();
+            rbtnStudent.Checked = false;
+            rbtnStaff.Checked = false;
+            txtAddress.Clear();
+            txtContactNumber.Clear();
+            txtGuardianName.Clear();
+            txtGuardianName.Visible = false;
+            lblGurdian.Visible = false;
+        }
+
+        public void radioButtonMemberType()
+        {
+
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=NIMTARA\SQLEXPRESS;Initial Catalog=library-management-system;Integrated Security=True");
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string memberType = "", status = "active";
+            int bookLimit = 0, returnDays = 0, issuedBooks = 0;
+          
+            if(rbtnStudent.Checked == true)
+            {
+                memberType = "Student";
+                bookLimit = 1;
+                returnDays = 7;
+            }
+            else if(rbtnStaff.Checked == true)
+            {
+                memberType = "Staff";
+                bookLimit = 5;
+                returnDays = 30;
+                txtGuardianName.Text = "";
+            }
+            if (rbtnStaff.Checked == false && rbtnStudent.Checked == false)
+            {
+                MessageBox.Show("Please select membertype");
+            }
+            else
+            {
+                try
+                {
+                    string query_insert = "insert into tblMember values ('" + txtMemberID.Text + "', '" + txtMemberName.Text + "', '" + memberType + "', '" + txtAddress.Text + "', '" + txtContactNumber.Text + "', '"+txtGuardianName.Text+"', '" + lblDateMM.Text + "', '" + lblUserMM.Text + "', '" + issuedBooks + "', '" + status + "', '" + bookLimit + "', '" + returnDays + "')";
+                    SqlCommand cmd = new SqlCommand(query_insert, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Member added successfully");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error while adding member " + ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+                clear();
+            }
+        }
+
+        private void rbtnStudent_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbtnStudent.Checked==true)
+            {
+                lblGurdian.Visible = true;
+                txtGuardianName.Visible = true;
+            }
+        }
+
+        private void rbtnStaff_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbtnStaff.Checked==true)
+            {
+                lblGurdian.Visible = false;
+                txtGuardianName.Visible = false;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string memberType = "";
+
+            if (rbtnStudent.Checked == true)
+            {
+                memberType = "Student";
+            }
+            else if (rbtnStaff.Checked == true)
+            {
+                memberType = "Staff";
+                txtGuardianName.Text = "";
+            }
+            try
+            {
+                string query_update = "update tblMember set mem_ID = '"+txtMemberID.Text+"', mem_name = '"+txtMemberName.Text+"', mem_type = '"+memberType+ "', mem_address = '"+txtAddress.Text+"', mem_telephone = '"+txtContactNumber.Text+ "', mem_guardian = '" + txtGuardianName.Text + "' where mem_ID = '" + txtMemberID.Text + "'";
+                SqlCommand cmd = new SqlCommand(query_update, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Member updated successfully");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error while updating member " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query_delete = "delete from tblMember where mem_ID = '" + txtMemberID.Text + "'";
+                SqlCommand cmd = new SqlCommand(query_delete, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Member deleted successfully");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error while deleting member " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnSearchMemberID_Click(object sender, EventArgs e)
+        {
+            string memberType = "";
+            try
+            {
+                string query_search = "select * from tblMember where mem_ID = '" + txtMemberID.Text + "'";
+                SqlCommand cmd = new SqlCommand(query_search, con);
+                con.Open();
+                SqlDataReader r = cmd.ExecuteReader();
+
+                while (r.Read())
+                {
+                    txtMemberID.Text = r[0].ToString();
+                    txtMemberName.Text = r[1].ToString();
+                    memberType = r[2].ToString();
+                    txtAddress.Text = r[3].ToString();
+                    txtContactNumber.Text = r[4].ToString();
+                    txtGuardianName.Text = r[5].ToString();
+                }
+
+                if(memberType == "Student")
+                {
+                    rbtnStudent.Checked = true;
+                }
+                else if(memberType == "Staff")
+                {
+                    rbtnStaff.Checked = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error while searching " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnSearchMemberName_Click(object sender, EventArgs e)
+        {
+            string memberType = "";
+            try
+            {
+                string query_search = "select * from tblMember where mem_name LIKE '%" + txtMemberName.Text + "%'";
+                SqlCommand cmd = new SqlCommand(query_search, con);
+                con.Open();
+                SqlDataReader r = cmd.ExecuteReader();
+
+                while (r.Read())
+                {
+                    txtMemberID.Text = r[0].ToString();
+                    txtMemberName.Text = r[1].ToString();
+                    memberType = r[2].ToString();
+                    txtAddress.Text = r[3].ToString();
+                    txtContactNumber.Text = r[4].ToString();
+                    txtGuardianName.Text = r[5].ToString();
+                }
+
+                if (memberType == "Student")
+                {
+                    rbtnStudent.Checked = true;
+                }
+                else if (memberType == "Staff")
+                {
+                    rbtnStaff.Checked = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while searching " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            frmHome home = new frmHome();
+            home.Visible = true;
+            this.Visible = false;
+            home.lblUserHome.Text = lblUserMM.Text;
+            home.lblUserTypeHome.Text = lblUserTypeMM.Text;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMemberManagement_Load(object sender, EventArgs e)
+        {
+            lblDateMM.Text = DateTime.Now.ToString("dd/MM/yyyy");
         }
     }
 }
